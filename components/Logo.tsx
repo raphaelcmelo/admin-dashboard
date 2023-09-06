0bf7ea3192651cd29670ac1f2b29e6adc7ceec0a
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-09-06 13:19:52 -0300

    first commit

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <h1 className="text-2x1 font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
+      AdminDashboard
+    </h1>
+  );
+}
+
+export default Logo;
